dd086b29115ba8abe9d22a5b8829f83b7a63192b
Caturra <wasdjkl233@gmail.com>
2024-11-08 21:27:07 +0800

    [stdexec] update examples: schedule once

--- experimental/stdexec/examples/echo_sender.cpp
+++ experimental/stdexec/examples/echo_sender.cpp
@@ -6,40 +6,38 @@
 #include "uring_exec.hpp"
 #include "utils.h"
 
-// TODO: request stop.
 stdexec::sender
 auto echo(io_uring_exec::scheduler scheduler, int client_fd) {
     return
-        stdexec::schedule(scheduler)
-      | stdexec::let_value([] {
-            return stdexec::just(std::array<char, 512>{});
-         })
+        stdexec::just(std::array<char, 512>{})
       | stdexec::let_value([=](auto &buf) {
             return
                 async_read(scheduler, client_fd, buf.data(), buf.size())
               | stdexec::then([=, &buf](int read_bytes) {
+                    auto copy = std::ranges::copy;
+                    auto view = buf | std::views::take(read_bytes);
                     auto to_console = std::ostream_iterator<char>{std::cout};
-                    std::ranges::copy(buf | std::views::take(read_bytes), to_console);
+                    copy(view, to_console);
                     return read_bytes;
                 })
               | stdexec::let_value([=, &buf](int read_bytes) {
                     return async_write(scheduler, client_fd, buf.data(), read_bytes);
+                })
+              | stdexec::let_value([=, &buf](int written_bytes) {
+                    return stdexec::just(written_bytes == 0 || buf[0] == '@');
                 });
         })
-      | stdexec::let_value([](int written_bytes) {
-            (void)written_bytes;
-            return stdexec::just(false);
-        })
-      | exec::repeat_effect_until();
+      | exec::repeat_effect_until()
+      | stdexec::let_value([=] {
+            std::cout << "Closing client..." << std::endl;
+            return async_close(scheduler, client_fd) | stdexec::then([](...){});
+        });
 }
 
 stdexec::sender
 auto server(io_uring_exec::scheduler scheduler, int server_fd, exec::async_scope &scope) {
     return
-        stdexec::schedule(scheduler)
-      | stdexec::let_value([=] {
-            return async_accept(scheduler, server_fd, {});
-        })
+        async_accept(scheduler, server_fd, {})
       | stdexec::let_value([=, &scope](int client_fd) {
             scope.spawn(echo(scheduler, client_fd));
             return stdexec::just(false);
@@ -54,8 +52,14 @@ int main() {
     io_uring_exec uring({.uring_entries=512});
     exec::async_scope scope;
 
+    auto scheduler = uring.get_scheduler();
+
     scope.spawn(
-        server(uring.get_scheduler(), server_fd, scope));
+        stdexec::schedule(scheduler)
+      | stdexec::let_value([=, &scope] {
+          return server(scheduler, server_fd, scope);
+      })
+    );
 
     uring.run();
 }
