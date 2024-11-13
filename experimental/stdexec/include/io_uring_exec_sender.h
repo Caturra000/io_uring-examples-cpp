79cf046453c465e2023191176997bd2271e35445
Caturra <wasdjkl233@gmail.com>
2024-11-13 16:35:00 +0800

    [stdexec] add async_nop

--- experimental/stdexec/include/io_uring_exec_sender.h
+++ experimental/stdexec/include/io_uring_exec_sender.h
@@ -31,7 +31,7 @@ struct io_uring_exec_sender {
 };
 
 template <auto io_uring_prep_invocable, typename ...Args>
-stdexec::sender_of<
+inline stdexec::sender_of<
     stdexec::set_value_t(io_uring_exec_operation_base::result_t /* cqe->res */),
     stdexec::set_error_t(std::exception_ptr)>
 auto make_uring_sender(std::in_place_t,
@@ -41,7 +41,7 @@ auto make_uring_sender(std::in_place_t,
 
 // A sender factory.
 template <auto io_uring_prep_invocable>
-auto make_uring_sender(io_uring_exec::scheduler s, auto &&...args) noexcept {
+inline auto make_uring_sender(io_uring_exec::scheduler s, auto &&...args) noexcept {
     return make_uring_sender<io_uring_prep_invocable>
         (std::in_place, s, std::tuple(static_cast<decltype(args)&&>(args)...));
 }
@@ -51,27 +51,27 @@ auto make_uring_sender(io_uring_exec::scheduler s, auto &&...args) noexcept {
 // async API, reading and updating the current file offset may result in unpredictable behavior, unless access to
 // the file is serialized. It is **not encouraged** to use this feature, if it's possible to provide the  desired  IO
 // offset from the application or library.
-stdexec::sender
+inline stdexec::sender
 auto async_read(io_uring_exec::scheduler s, int fd, void *buf, size_t n, uint64_t offset = 0) noexcept {
     return make_uring_sender<io_uring_prep_read>(s, fd, buf, n, offset);
 }
 
-stdexec::sender
+inline stdexec::sender
 auto async_write(io_uring_exec::scheduler s, int fd, const void *buf, size_t n, uint64_t offset = 0) noexcept {
     return make_uring_sender<io_uring_prep_write>(s, fd, buf, n, offset);
 }
 
-stdexec::sender
+inline stdexec::sender
 auto async_close(io_uring_exec::scheduler s, int fd) noexcept {
     return make_uring_sender<io_uring_prep_close>(s, fd);
 }
 
-stdexec::sender
+inline stdexec::sender
 auto async_accept(io_uring_exec::scheduler s,int fd, int flags) noexcept {
     return make_uring_sender<io_uring_prep_accept>(s, fd, nullptr, nullptr, flags);
 }
 
-stdexec::sender
+inline stdexec::sender
 auto async_wait(io_uring_exec::scheduler s, std::chrono::milliseconds duration) noexcept {
     using namespace std::chrono;
     auto duration_s = duration_cast<seconds>(duration);
@@ -88,4 +88,11 @@ auto async_wait(io_uring_exec::scheduler s, std::chrono::milliseconds duration) 
                     return std::tuple<Ts...>{static_cast<decltype(args)&&>(args)...};
                 }(io_uring_prep_timeout, &ts, 0, 0));
         });
+}
+
+// Debug only.
+// The return value makes no sense at all.
+inline stdexec::sender
+auto async_nop(io_uring_exec::scheduler s, ...) noexcept {
+    return make_uring_sender<io_uring_prep_nop>(s);
 }
